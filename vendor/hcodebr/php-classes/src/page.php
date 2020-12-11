f36d274f11351089dd9ab60d6069c99ca7ccf423
Diego Lima <diegolima.sao@gmail.com>
2020-12-11 14:13:36 -0300

    criando classe pageAdmin e tplAdmin

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -4,7 +4,7 @@ namespace Hcode;
 
 use Rain\Tpl;
 
-class page{
+class Page{
 
     private $tpl;
     private $options = [];
@@ -12,12 +12,12 @@ class page{
         "data" => []
     ];
 
-    public function __construct($opts = array()){
+    public function __construct($opts = array(), $tpl_dir = "/views/"){
 
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false
            );
